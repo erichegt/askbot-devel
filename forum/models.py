425e4e5d6edfbfa93a8e940ebae26e8d1dc1292b
Bruno Sarlo <bruno@bruno-laptop.(none)>
2009-08-11 15:49:31 -0300

    Added unicode encoding for tags.

--- forum/models.py
+++ forum/models.py
@@ -180,7 +180,7 @@ class Question(models.Model):
         return [name for name in self.tagnames.split(u' ')]
 
     def tagname_meta_generator(self):
-        return u','.join([str(tag) for tag in self.tagname_list()])
+        return u','.join([unicode(tag) for tag in self.tagname_list()])
 
     def get_absolute_url(self):
         return '%s%s' % (reverse('question', args=[self.id]), self.title.replace(' ', '-'))
