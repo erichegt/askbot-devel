ef13ff40fdb71b37a4a75056e02bb31b32fc58e4
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2009-11-25 19:44:46 -0500

    fixed user tag selection and count, link points to user authored questions/answers, fixed diff_date a little

--- forum/templatetags/extra_tags.py
+++ forum/templatetags/extra_tags.py
@@ -251,7 +251,7 @@ def diff_date(date, limen=2):
         return _('2 days ago')
     elif days == 1:
         return _('yesterday')
-    elif minutes > 60:
+    elif minutes >= 60:
         return ungettext('%(hr)d hour ago','%(hr)d hours ago',hours) % {'hr':hours}
     else:
         return ungettext('%(min)d min ago','%(min)d mins ago',minutes) % {'min':minutes}
