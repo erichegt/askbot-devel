39d375d05ecb6a5e12f8481c8ff899939f8bf468
hrcerqueira <hrcerqueira@gmail.com>
2010-01-22 20:54:43 +0000

    Handled the returning user case, and the proper redirects in the case of an anonymous question or answer.

--- urls.py
+++ urls.py
@@ -2,6 +2,10 @@ from django.conf.urls.defaults import *
 from django.utils.translation import ugettext as _
 import settings
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
     (r'^%s' % settings.FORUM_SCRIPT_ALIAS, include('forum.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
