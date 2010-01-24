4e7d6e104d95115b091e6bbf29148b5b1c96aa7e
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2010-01-24 14:12:22 -0500

    dos2unix everything, added dos2unix.sh script

---
+++ dos2unix.sh
@@ -0,0 +1,10 @@
+dos2unix `find . -name '*.py'`
+dos2unix `find . -name '*.po'`
+dos2unix `find . -name '*.js'`
+dos2unix `find . -name '*.css'`
+dos2unix `find . -name '*.txt'`
+dos2unix `find ./sphinx -type f`
+dos2unix `find ./cron -type f`
+dos2unix settings_local.py.dist
+dos2unix README
+dos2unix INSTALL
