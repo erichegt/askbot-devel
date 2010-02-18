2ea80d9c8d6c9e6ace51c59ebfc194b59bb0480a
hrcerqueira <hrcerqueira@gmail.com>
2010-02-18 00:34:23 +0000

    some more removals

--- tester/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Tag(models.Model):
-    name            = models.CharField(max_length=255, unique=True)
-
-class Question(models.Model):
-    title    = models.CharField(max_length=300)
-    tags     = models.ManyToManyField(Tag, related_name='questions')
