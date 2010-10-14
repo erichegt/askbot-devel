0a082c0dde3d8d558f157836824735f18d373ca3
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2010-10-14 19:42:59 -0400

    fixed indentation error

--- askbot/models/content.py
+++ askbot/models/content.py
@@ -273,15 +273,15 @@ class Content(models.Model):
                     )
 
     def post_get_last_update_info(self):#todo: rename this subroutine
-            when = self.added_at
-            who = self.author
-            if self.last_edited_at and self.last_edited_at > when:
-                when = self.last_edited_at
-                who = self.last_edited_by
-            comments = self.comments.all()
-            if len(comments) > 0:
-                for c in comments:
-                    if c.added_at > when:
-                        when = c.added_at
-                        who = c.user
-            return when, who
+        when = self.added_at
+        who = self.author
+        if self.last_edited_at and self.last_edited_at > when:
+            when = self.last_edited_at
+            who = self.last_edited_by
+        comments = self.comments.all()
+        if len(comments) > 0:
+            for c in comments:
+                if c.added_at > when:
+                    when = c.added_at
+                    who = c.user
+        return when, who
