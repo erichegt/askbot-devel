0aade4817a07392b874005180d815266cf6521ba
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2010-10-25 15:00:44 -0400

    included creation of procedural language plpgsql if it is needed to simplify postgres db creation

--- askbot/management/commands/init_postgresql_full_text_search.py
+++ askbot/management/commands/init_postgresql_full_text_search.py
@@ -8,10 +8,16 @@ class Command(NoArgsCommand):
         dir = os.path.dirname(__file__)
         sql_file_name = 'setup_postgresql_full_text_search.plsql'
         sql_file_name = os.path.join(dir, sql_file_name)
-        query = open(sql_file_name).read()
+        fts_init_query = open(sql_file_name).read()
 
         cursor = connection.cursor()
         try:
-            cursor.execute(query)
+            #test if language exists
+            cursor.execute("SELECT * FROM pg_language WHERE lanname='plpgsql'")
+            lang_exists = cursor.fetchone()
+            if not lang_exists:
+                cursor.execute("CREATE LANGUAGE plpgsql")
+            #run the main query
+            cursor.execute(fts_init_query)
         finally:
             cursor.close()
