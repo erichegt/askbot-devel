0a1f402e2a585445d0f75b0d105e62cc0fa7b970
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-02-09 22:27:44 -0500

    fixed a bug where askbot would not work without the avatar app, and excluded avatar and adzone apps from the distribution

--- askbot/views/__init__.py
+++ askbot/views/__init__.py
@@ -6,4 +6,6 @@ from askbot.views import writers
 from askbot.views import commands
 from askbot.views import users
 from askbot.views import meta
-from askbot.views import avatar_views
+from django.conf import settings
+if 'avatar' in settings.INSTALLED_APPS:
+    from askbot.views import avatar_views
