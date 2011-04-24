020c72b0b964507e64d3e25ce974b3916dcdb736
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-04-24 18:31:22 -0400

    bumped version

--- askbot/__init__.py
+++ askbot/__init__.py
@@ -11,7 +11,7 @@ import logging
 from askbot import patches
 from askbot.deployment.assertions import assert_package_compatibility
 
-VERSION = (0, 6, 77)
+VERSION = (0, 6, 78)
 
 #necessary for interoperability of django and coffin
 assert_package_compatibility()
