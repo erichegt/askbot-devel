139f8ed96156aa755a83c7a38ba4f28a51611657
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-06-15 04:06:19 -0400

    allowed more characters in the user name

--- askbot/const/__init__.py
+++ askbot/const/__init__.py
@@ -215,7 +215,7 @@ NOTIFICATION_DELIVERY_SCHEDULE_CHOICES= (
                         )
 
 USERS_PAGE_SIZE = 28#todo: move it to settings?
-USERNAME_REGEX_STRING = r'^[\w \-]+$'
+USERNAME_REGEX_STRING = r'^[\w \-.@+\']+$'
 
 #chars that can go before or after @mention
 TWITTER_STYLE_MENTION_TERMINATION_CHARS = '\n ;:,.!?<>"\''
