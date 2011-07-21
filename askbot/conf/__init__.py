3d414aa3a846090009698d2fcdc9ea68418e7ea5
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-07-21 16:45:47 -0400

    removed the unused grapefruit module

--- askbot/conf/__init__.py
+++ askbot/conf/__init__.py
@@ -8,7 +8,6 @@ import askbot.conf.forum_data_rules
 import askbot.conf.flatpages
 import askbot.conf.site_settings
 import askbot.conf.external_keys
-#import askbot.conf.skin_counter_settings
 import askbot.conf.skin_general_settings
 import askbot.conf.user_settings
 import askbot.conf.markup
