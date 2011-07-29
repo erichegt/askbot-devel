e7c418296189bbb9d57f7450de61459d3af7b412
NoahY <yuttadhammo@yahoo.com>
2011-07-29 13:20:55 +0530

    sidebar settings for each view

---
+++ askbot/conf/sidebar_profile.py
@@ -0,0 +1,45 @@
+"""
+Sidebar settings
+"""
+from askbot.conf.settings_wrapper import settings
+from askbot.deps.livesettings import ConfigurationGroup
+from askbot.deps.livesettings import values
+from django.utils.translation import ugettext as _
+
+SIDEBAR_PROFILE = ConfigurationGroup(
+                    'SIDEBAR_PROFILE',
+                    _('Sidebar widget settings - profile page'),
+                )
+
+settings.register(
+    values.LongStringValue(
+        SIDEBAR_PROFILE,
+        'SIDEBAR_PROFILE_HEADER',
+        description = _('Custom sidebar header'),
+        default = '',
+        help_text = _(
+                    'Use this area to enter content at the TOP of the sidebar'
+                    'in HTML format.   When using this option '
+                    '(as well as the sidebar footer), please '
+                    'use the HTML validation service to make sure that '
+                    'your input is valid and works well in all browsers.'
+                    )
+    )
+)
+
+settings.register(
+    values.LongStringValue(
+        SIDEBAR_PROFILE,
+        'SIDEBAR_PROFILE_FOOTER',
+        description = _('Custom sidebar footer'),
+        default = '',
+        help_text = _(
+                    'Use this area to enter content at the BOTTOM of the sidebar'
+                    'in HTML format.   When using this option '
+                    '(as well as the sidebar header), please '
+                    'use the HTML validation service to make sure that '
+                    'your input is valid and works well in all browsers.'
+                    )
+    )
+)
+
---
+++ askbot/conf/sidebar_question.py
@@ -0,0 +1,85 @@
+"""
+Sidebar settings
+"""
+from askbot.conf.settings_wrapper import settings
+from askbot.deps.livesettings import ConfigurationGroup
+from askbot.deps.livesettings import values
+from django.utils.translation import ugettext as _
+
+SIDEBAR_QUESTION = ConfigurationGroup(
+                    'SIDEBAR_QUESTION',
+                    _('Sidebar widget settings - question page'),
+                )
+
+settings.register(
+    values.LongStringValue(
+        SIDEBAR_QUESTION,
+        'SIDEBAR_QUESTION_HEADER',
+        description = _('Custom sidebar header'),
+        default = '',
+        help_text = _(
+                    'Use this area to enter content at the TOP of the sidebar'
+                    'in HTML format.   When using this option '
+                    '(as well as the sidebar footer), please '
+                    'use the HTML validation service to make sure that '
+                    'your input is valid and works well in all browsers.'
+                    )
+    )
+)
+
+settings.register(
+    values.BooleanValue(
+        SIDEBAR_QUESTION,
+        'SIDEBAR_QUESTION_SHOW_TAGS',
+        description = _('Show tag list in sidebar'),
+        help_text = _(
+                    'Uncheck this if you want to hide the tag '
+                    'list from the sidebar ' 
+                    ),
+        default = True
+    )
+)
+
+settings.register(
+    values.BooleanValue(
+        SIDEBAR_QUESTION,
+        'SIDEBAR_QUESTION_SHOW_META',
+        description = _('Show meta information in sidebar'),
+        help_text = _(
+                    'Uncheck this if you want to hide the meta ' 
+                    'information about the question (post date, ' 
+                    'views, last updated). ' 
+                    ),
+        default = True
+    )
+)
+
+settings.register(
+    values.BooleanValue(
+        SIDEBAR_QUESTION,
+        'SIDEBAR_QUESTION_SHOW_RELATED',
+        description = _('Show related questions in sidebar'),
+        help_text = _(
+                    'Uncheck this if you want to hide the list ' 
+                    'of related questions. ' 
+                    ),
+        default = True
+    )
+)
+
+settings.register(
+    values.LongStringValue(
+        SIDEBAR_QUESTION,
+        'SIDEBAR_QUESTION_FOOTER',
+        description = _('Custom sidebar footer'),
+        default = '',
+        help_text = _(
+                    'Use this area to enter content at the BOTTOM of the sidebar'
+                    'in HTML format.   When using this option '
+                    '(as well as the sidebar header), please '
+                    'use the HTML validation service to make sure that '
+                    'your input is valid and works well in all browsers.'
+                    )
+    )
+)
+
