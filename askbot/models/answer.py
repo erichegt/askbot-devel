e44e933836dea8bd8498bd116c739d5715121720
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-07-31 15:16:34 -0400

    a minor change

--- askbot/models/answer.py
+++ askbot/models/answer.py
@@ -289,7 +289,7 @@ class Answer(content.Content, DeletableContent):
         return self.question.title
 
     def get_absolute_url(self):
-        return '%(base)s%(slug)s?answer=%(id)d#%(id)d' % \
+        return u'%(base)s%(slug)s?answer=%(id)d#%(id)d' % \
                 {
                     'base': reverse('question', args=[self.question.id]),
                     'slug': django_urlquote(slugify(self.question.title)),
