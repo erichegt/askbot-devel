ad03c27cb73509a149ef8680f753bd80849cac7f
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-08-31 12:19:19 -0300

    fixed indentation issues

--- askbot/utils/hasher.py
+++ askbot/utils/hasher.py
@@ -5,35 +5,36 @@ from askbot.skins.utils import get_path_to_skin, get_skin_choices
 
 
 def get_hash_of_dirs(directory):
-  SHAhash = hashlib.sha1()
-  if not os.path.exists (directory):
-    return -1
-    
-  try:
-    for root, dirs, files in os.walk(directory):
-      for names in files:
-        filepath = os.path.join(root,names)
-        try:
-          f1 = open(filepath, 'rb')
-        except:
-          # You can't open the file for some reason
-          f1.close()
-          continue
+    '''Hasher function for a directory and its files'''
+    sha_hash = hashlib.sha1()
+    if not os.path.exists (directory):
+        return -1
+      
+    try:
+        for root, dirs, files in os.walk(directory):
+            for names in files:
+                filepath = os.path.join(root, names)
+                try:
+                    f1 = open(filepath, 'rb')
+                except:
+                    # You can't open the file for some reason
+                    f1.close()
+                    continue
 
-	while 1:
-	  # Read file in as little chunks
-  	  buf = f1.read(4096)
-	  if not buf : break
-	  SHAhash.update(hashlib.sha1(buf).hexdigest())
-        f1.close()
+        while 1:
+            # Read file in as little chunks
+            buf = f1.read(4096)
+            if not buf : break
+            sha_hash.update(hashlib.sha1(buf).hexdigest())
+            f1.close()
 
-  except:
-    import traceback
-    # Print the stack traceback
-    traceback.print_exc()
-    return -2
+    except:
+        import traceback
+        # Print the stack traceback
+        traceback.print_exc()
+        return -2
 
-  return SHAhash.hexdigest()
+    return sha_hash.hexdigest()
 
 def update_revision(skin = None):
     resource_revision = askbot_settings.MEDIA_RESOURCE_REVISION
