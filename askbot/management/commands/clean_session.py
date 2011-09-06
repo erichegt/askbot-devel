2273b2555a977ae365bee51a0bffdb645a445544
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-09-06 16:13:27 -0300

    added quiet parameter to clear_session command

--- askbot/management/commands/clean_session.py
+++ askbot/management/commands/clean_session.py
@@ -1,22 +1,31 @@
 from django.core.management.base import NoArgsCommand
 from django.contrib.sessions.models import Session
 from django.db import transaction
+from optparse import make_option
 from askbot.utils.console import print_progress
 from datetime import datetime
 
 DELETE_LIMIT = 1000
 
 class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+            make_option('--quiet',
+                action='store_true',
+                dest='quiet',
+                default=False,
+                help="Do not print anything when called."
+                ),
+            )
 
     @transaction.commit_manually
     def handle_noargs(self, **options):
         '''deletes old sessions'''
-        verbosity = options.get('verbosity', '0')
+        quiet = options.get('quiet', False)
         expired_session_count  = Session.objects.filter(expire_date__lt=datetime.now()).count()
         expired_session_list= Session.objects.filter(expire_date__lt=datetime.now()).values_list('session_key', flat=True)
         transaction.commit()
 
-        if verbosity > '1':
+        if not quiet:
             print "There are %d expired sessions" % expired_session_count
 
         range_limit = len(expired_session_list) - 1
@@ -28,7 +37,7 @@ class Command(NoArgsCommand):
             sublist = expired_session_list[lower_limit:higher_limit]
             Session.objects.filter(session_key__in = sublist).delete()
             transaction.commit()
-            if verbosity > '1':
+            if not quiet:
                 print_progress(higher_limit-1, expired_session_count)
 
         if higher_limit < expired_session_list:
@@ -37,5 +46,5 @@ class Command(NoArgsCommand):
             print_progress(expired_session_count, expired_session_count)
             transaction.commit()
 
-        if verbosity > '1':
+        if not quiet:
             print "sessions cleared"
