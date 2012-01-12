4b3bc55042499ca9096c4d1ed4a65ad36cd74868
Tomasz Zielinski <tomasz.zielinski@pyconsultant.eu>
2012-01-12 23:45:19 +0100

    Bugfixes to the previous commit

--- askbot/conf/__init__.py
+++ askbot/conf/__init__.py
@@ -31,5 +31,4 @@ def should_show_sort_by_relevance():
     """True if configuration support sorting
     questions by search relevance
     """
-    return True
-    #return ('postgresql_psycopg2' in askbot.get_database_engine_name())
+    return ('postgresql_psycopg2' in askbot.get_database_engine_name())
