45197a2dbf0f4398c7e79b330513e05e224cfe9a
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2012-03-14 13:20:53 -0400

    recompiled locales

--- askbot/conf/user_settings.py
+++ askbot/conf/user_settings.py
@@ -82,7 +82,7 @@ settings.register(
         default = True,
         description = _('Use automatic avatars from gravatar.com'),
         help_text=_(
-            'Check this option if you want to allow the use of gravatar.com for avatars. Please, note that this feature might take about 10 minutes to become 100% effective. You will have to enable uploaded avatars as well. For more information, please visit <a href="http://askbot.org/doc/optional-modules.html#uploaded-avatars">this page</a>.'
+            'Check this option if you want to allow the use of gravatar.com for avatars. Please, note that this feature might take about 10 minutes to become fully effective. You will have to enable uploaded avatars as well. For more information, please visit <a href="http://askbot.org/doc/optional-modules.html#uploaded-avatars">this page</a>.'
         ) 
     )
 )
