32a4a5ddf93f2bc605706ecc3eafbc63dc923acf
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2012-05-10 09:53:48 -0600

    fixed preload_cache command

--- askbot/management/commands/preload_livesettings_cache.py
+++ askbot/management/commands/preload_livesettings_cache.py
@@ -1,17 +1,12 @@
 from django.core.management.base import NoArgsCommand
-from askbot.conf import settings
 
 class Command(NoArgsCommand):
     '''Loads livesettings values to cache helping speed up
        initial load time for the users'''
 
     def handle_noargs(self, **options):
-        empty1 = emtpy2 = None
-        print 'loading cache'
+        from askbot.conf import settings
         #Just loads all the settings that way they will be in the cache
         for key, value in settings._ConfigSettings__instance.items():
-            empty1 = key
-            empty2 = value
+            empty1 = getattr(settings, key)
         print 'cache pre-loaded'
-
-
