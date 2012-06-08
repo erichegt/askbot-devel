760cee24ab84483b48c3a3565857edd1f9a48b73
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2012-06-07 21:35:56 -0400

    swapped default setting values with the bootstrap mode and renamed bootstrap mode to "large site mode"

--- askbot/conf/site_modes.py
+++ askbot/conf/site_modes.py
@@ -9,35 +9,35 @@ from askbot.conf.super_groups import REP_AND_BADGES
 from askbot.deps.livesettings import ConfigurationGroup, BooleanValue
 from django.utils.translation import ugettext as _
 
-BOOTSTRAP_MODE_SETTINGS = {
+LARGE_SITE_MODE_SETTINGS = {
     #minimum reputation settins.
-    'MIN_REP_TO_VOTE_UP': 5,
-    'MIN_REP_TO_VOTE_DOWN': 50,
-    'MIN_REP_TO_ANSWER_OWN_QUESTION': 5,
-    'MIN_REP_TO_ACCEPT_OWN_ANSWER': 20,
-    'MIN_REP_TO_FLAG_OFFENSIVE': 5,
-    'MIN_REP_TO_LEAVE_COMMENTS': 10,
-    'MIN_REP_TO_DELETE_OTHERS_COMMENTS': 200,
-    'MIN_REP_TO_DELETE_OTHERS_POSTS': 500,
-    'MIN_REP_TO_UPLOAD_FILES': 10,
-    'MIN_REP_TO_CLOSE_OWN_QUESTIONS': 25,
-    'MIN_REP_TO_RETAG_OTHERS_QUESTIONS': 50,
-    'MIN_REP_TO_REOPEN_OWN_QUESTIONS': 50,
-    'MIN_REP_TO_EDIT_WIKI': 75,
-    'MIN_REP_TO_EDIT_OTHERS_POSTS': 200,
-    'MIN_REP_TO_VIEW_OFFENSIVE_FLAGS': 200,
-    'MIN_REP_TO_CLOSE_OTHERS_QUESTIONS': 200,
-    'MIN_REP_TO_LOCK_POSTS': 400,
-    'MIN_REP_TO_HAVE_STRONG_URL': 25,
+    'MIN_REP_TO_VOTE_UP': 15,
+    'MIN_REP_TO_VOTE_DOWN': 100,
+    'MIN_REP_TO_ANSWER_OWN_QUESTION': 25,
+    'MIN_REP_TO_ACCEPT_OWN_ANSWER': 50,
+    'MIN_REP_TO_FLAG_OFFENSIVE': 15,
+    'MIN_REP_TO_LEAVE_COMMENTS': 50,
+    'MIN_REP_TO_DELETE_OTHERS_COMMENTS': 2000,
+    'MIN_REP_TO_DELETE_OTHERS_POSTS': 5000,
+    'MIN_REP_TO_UPLOAD_FILES': 60,
+    'MIN_REP_TO_CLOSE_OWN_QUESTIONS': 250,
+    'MIN_REP_TO_RETAG_OTHERS_QUESTIONS': 500,
+    'MIN_REP_TO_REOPEN_OWN_QUESTIONS': 500,
+    'MIN_REP_TO_EDIT_WIKI': 750,
+    'MIN_REP_TO_EDIT_OTHERS_POSTS': 2000,
+    'MIN_REP_TO_VIEW_OFFENSIVE_FLAGS': 2000,
+    'MIN_REP_TO_CLOSE_OTHERS_QUESTIONS': 2000,
+    'MIN_REP_TO_LOCK_POSTS': 4000,
+    'MIN_REP_TO_HAVE_STRONG_URL': 250,
     #badge settings
-    'NOTABLE_QUESTION_BADGE_MIN_VIEWS': 25,
-    'POPULAR_QUESTION_BADGE_MIN_VIEWS': 15,
-    'FAMOUS_QUESTION_BADGE_MIN_VIEWS': 50,
-    'ENTHUSIAST_BADGE_MIN_DAYS': 5,
-    'TAXONOMIST_BADGE_MIN_USE_COUNT': 5,
+    'NOTABLE_QUESTION_BADGE_MIN_VIEWS': 250,
+    'POPULAR_QUESTION_BADGE_MIN_VIEWS': 150,
+    'FAMOUS_QUESTION_BADGE_MIN_VIEWS': 500,
+    'ENTHUSIAST_BADGE_MIN_DAYS': 30,
+    'TAXONOMIST_BADGE_MIN_USE_COUNT': 10,
     #moderation rule settings
-    'MIN_FLAGS_TO_HIDE_POST': 2,
-    'MIN_FLAGS_TO_DELETE_POST': 3,
+    'MIN_FLAGS_TO_HIDE_POST': 3,
+    'MIN_FLAGS_TO_DELETE_POST': 5,
 }
 
 def bootstrap_callback(current_value, new_value):
@@ -49,11 +49,11 @@ def bootstrap_callback(current_value, new_value):
         return new_value
 
     if new_value == True:
-        for key, value in BOOTSTRAP_MODE_SETTINGS.items():
+        for key, value in LARGE_SITE_MODE_SETTINGS.items():
             settings.update(key, value)
 
     else:
-        for key in BOOTSTRAP_MODE_SETTINGS:
+        for key in LARGE_SITE_MODE_SETTINGS:
             settings.reset(key)
 
     return new_value
@@ -68,14 +68,14 @@ SITE_MODES = ConfigurationGroup(
 settings.register(
     BooleanValue(
         SITE_MODES,
-        'ACTIVATE_BOOTSTRAP_MODE',
+        'ACTIVATE_LARGE_SITE_MODE',
         default=False,
         description=_(
-            'Activate a "Bootstrap" mode'),
+            'Activate a "Large site" mode'),
         help_text=_(
-            "Bootstrap mode lowers reputation and certain badge "
+            "\"Large site\" mode increases reputation and certain badge "
             "thresholds, to values, more suitable "
-            "for the smaller communities, "
+            "for the larger communities, "
             "<strong>WARNING:</strong> your current values for "
             "Minimum reputation, "
             "Badge Settings and "
