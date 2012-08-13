cb9ca52d3549eaf686a9079212ce26976f147dfa
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2012-08-13 13:19:31 -0600

    Fixed widget styles.

--- askbot/skins/default/templates/widgets/askbot_widget.js
+++ askbot/skins/default/templates/widgets/askbot_widget.js
@@ -14,6 +14,9 @@ var AskbotAskWidget = {
     var motherDiv = document.createElement('div');
     motherDiv.setAttribute("id", AskbotAskWidget.element_id);
 
+    var containerDiv = document.createElement('div');
+    motherDiv.appendChild(containerDiv);
+
     {%if widget.outer_style %}
     outerStyle = document.createElement('style');
     outerStyle.innerText = "{{widget.outer_style}}";
@@ -26,17 +29,16 @@ var AskbotAskWidget = {
     closeButton.setAttribute('onClick', 'AskbotAskWidget.widgetToggle();');
     closeButton.innerText = 'Close';
 
-    motherDiv.appendChild(closeButton);
+    containerDiv.appendChild(closeButton);
 
     var iframe = document.createElement('iframe');
     iframe.setAttribute('src', 'http://{{host}}{% url ask_by_widget widget.id %}');
 
-    motherDiv.appendChild(iframe);
+    containerDiv.appendChild(iframe);
 
     var body = document.getElementsByTagName('body')[0];
     if (body){
       body.insertBefore(motherDiv, body.firstChild);
-      body.insertBefore(AskbotAskWidget.createButton(), body.firstChild);
       body.insertBefore(link, body.firstChild);
     }
   },
@@ -45,8 +47,7 @@ var AskbotAskWidget = {
     var buttonDiv = document.createElement('div');
     buttonDiv.setAttribute('id', "AskbotAskButton");
 
-    var closeButton = document.createElement('a');
-    closeButton.setAttribute('href', '#');
+    var closeButton = document.createElement('button');
     closeButton.setAttribute('onClick', 'AskbotAskWidget.widgetToggle();');
     closeButton.innerText = label;
 
@@ -58,3 +59,4 @@ var AskbotAskWidget = {
 
 
 window.onload = AskbotAskWidget.toHtml;
+document.write(AskbotAskWidget.createButton().outerHTML);
