a01eb8e72289f4aa995315336806204abe873f48
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2012-08-20 18:53:13 -0600

    added cache to the view

--- askbot/views/widgets.py
+++ askbot/views/widgets.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, Http404
 from django.views.decorators import csrf
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 from django.contrib.auth.decorators import login_required
 
@@ -191,7 +192,7 @@ def delete_widget(request, model, widget_id):
         return render_into_skin('embed/delete_widget.html',
                 {'widget': widget, 'widget_name': model}, request)
 
-#TODO: Add cache
+@cache_page(60*30)
 def render_ask_widget_js(request, widget_id):
     widget = get_object_or_404(models.AskWidget, pk=widget_id)
     variable_name = "AskbotAskWidget%d" % widget.id
@@ -202,7 +203,7 @@ def render_ask_widget_js(request, widget_id):
     content =  content_tpl.render(Context(context_dict))
     return HttpResponse(content, mimetype='text/javascript')
 
-#TODO: Add cache
+@cache_page(60*30)
 def render_ask_widget_css(request, widget_id):
     widget = get_object_or_404(models.AskWidget, pk=widget_id)
     variable_name = "AskbotAskWidget%d" % widget.id
@@ -213,7 +214,7 @@ def render_ask_widget_css(request, widget_id):
     content =  content_tpl.render(Context(context_dict))
     return HttpResponse(content, mimetype='text/css')
 
-#search widget
+@cache_page(60*30)
 def question_widget(request, widget_id):
     """Returns the first x questions based on certain tags.
     @returns template with those questions listed."""
