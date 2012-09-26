3fdc78352a7a80451944af75c46e1cde790a0033
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2012-09-25 21:12:13 -0600

    temporal fix for email images issue with tinymce editor

--- askbot/tests/templatefilter_tests.py
+++ askbot/tests/templatefilter_tests.py
@@ -6,12 +6,12 @@ class AbsolutizeUrlsTests(TestCase):
     def setUp(self):
         askbot_settings.update('APP_URL', 'http://example.com')
     def test_absolutize_image_urls(self):
-        text = """<img class="junk" src="/some.gif"> <IMG SRC='/some.png'>"""
+        text = """<img class="junk" src="/some.gif"> <img class="junk" src="../../cat.gif"> <IMG SRC='/some.png'>"""
         #jinja register.filter decorator works in a weird way
         output = filters.absolutize_urls[0](text)
         self.assertEqual(
             output,
-            '<img class="junk" src="http://example.com/some.gif" style="max-width:500px;"> <IMG SRC="http://example.com/some.png" style="max-width:500px;">'
+            '<img class="junk" src="http://example.com/some.gif" style="max-width:500px;"> <img class="junk" src="http://example.com/../../cat.gif" style="max-width:500px;"> <IMG SRC="http://example.com/some.png" style="max-width:500px;">'
         )
     def test_absolutize_anchor_urls(self):
         text = """<a class="junk" href="/something">link</a> <A HREF='/something'>link</A>"""
