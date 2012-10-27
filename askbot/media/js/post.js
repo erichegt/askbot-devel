c7f777097631c9132fcf5ecbe90c7d85ea857eb7
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2012-10-27 16:20:49 -0300

    fixed an alert text on canceling the comment

--- askbot/media/js/post.js
+++ askbot/media/js/post.js
@@ -1591,7 +1591,7 @@ EditCommentForm.prototype.reset = function(){
 EditCommentForm.prototype.confirmAbandon = function(){
     this.focus(true);
     this._textarea.addClass('highlight');
-    var answer = confirm(gettext('confirm abandon comment'));
+    var answer = confirm(gettext("Are you sure you don't want to post this comment?"));
     this._textarea.removeClass('highlight');
     return answer;
 };
